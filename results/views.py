b928ff26278efc58abb8ed31c8dc716828bf873d
Daniel P Reardon <danreardon@gmail.com>
2016-07-22 13:55:59 -0400

    Improve the display page for the results

--- results/views.py
+++ results/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from .models import Drawing
+from django.db.models import Count, Sum
+from .models import Drawing, PrizesWon
 
 
 def index(request):
-    allDrawings = Drawing.objects.order_by('-drawingDate')[:100]
-    context = {'allDrawings': allDrawings}
+    allDrawings = Drawing.objects.order_by('-drawingDate')[:50]
+    allPrizes = PrizesWon.objects.order_by('-drawing')
+    toBePaid = PrizesWon.objects.aggregate(Sum('groupPrizeAmount'))
+    context = {'allDrawings': allDrawings, 'allPrizes': allPrizes, 'toBePaid':toBePaid}
     return render(request, 'results/index.html', context)
