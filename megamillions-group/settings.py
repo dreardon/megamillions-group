2258e3dd73569cfaa5a51c44ad62a2f39e617fa7
Daniel P Reardon <danreardon@gmail.com>
2016-07-23 16:46:42 -0400

    Update Display & Add Test

--- megamillions-group/settings.py
+++ megamillions-group/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'results',
     'debug_toolbar',
+    'django.contrib.humanize',
 )
 
 MIDDLEWARE_CLASSES = (
--- results/tests.py
+++ results/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import MegaNumbers, Drawing
 
-# Create your tests here.
+class ResultsTestCase(TestCase):
+    def setUp(self):
+        a = MegaNumbers(numbers='01,02,03,04,05',megaBall=6)
+        a.save()
+        b = MegaNumbers(numbers='02,03,04,05,06',megaBall=7)
+        b.save()
+        Drawing.objects.create(megaNumbers=a, multiplier=4,drawingDate='2016-07-22')
+        Drawing.objects.create(megaNumbers=b, multiplier=3,drawingDate='2016-07-23')
+
+    def test_MegaNumbers_saved(self):
+        c = MegaNumbers(numbers='03,04,05,06,07',megaBall=8)
+        c.save()
+        Drawing.objects.create(megaNumbers=c, multiplier=3,drawingDate='2016-07-24')
+        drawing = Drawing.objects.get(drawingDate='2016-07-24')
+        self.assertEqual(drawing.multiplier, 3)
+
+    def test_drawing_saved(self):
+        drawing = Drawing.objects.get(drawingDate='2016-07-22')
+        self.assertEqual(drawing.multiplier, 4)
+        drawing = Drawing.objects.get(drawingDate='2016-07-23')
+        self.assertEqual(drawing.multiplier, 3)
