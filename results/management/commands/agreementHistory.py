b40f040b7e9e4f975414a33b0fc14ddb7298497b
Daniel P Reardon <danreardon@gmail.com>
2017-02-19 20:49:09 -0500

    Change agreement dates

--- results/management/commands/agreementHistory.py
+++ results/management/commands/agreementHistory.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         AgreementPeriod(periodName="1st",startDate="2016-08-09",endDate="2016-11-16").save()
         AgreementPeriod(periodName="2nd",startDate="2016-11-22",endDate="2017-02-15").save()
-        AgreementPeriod(periodName="3rd",startDate="2017-02-19",endDate="2017-08-20").save()
+        AgreementPeriod(periodName="3rd",startDate="2017-02-21",endDate="2017-08-18").save()
 
         GroupTicket(active=False,numbers='04 08 23 36 50',megaBall=5,agreementPeriod=AgreementPeriod.objects.get(pk=1),autoPick=False).save()
         GroupTicket(active=False,numbers='03 07 13 24 40',megaBall=3,agreementPeriod=AgreementPeriod.objects.get(pk=1),autoPick=False).save()
@@ -40,4 +40,4 @@ class Command(BaseCommand):
         GroupTicket(active=True,numbers='12 22 43 50 56',megaBall=15,agreementPeriod=AgreementPeriod.objects.get(pk=3),autoPick=True).save()
 
         PaidOut(agreementPeriod=AgreementPeriod.objects.get(pk=1),prizeAmount="15").save()
-        PaidOut(agreementPeriod=AgreementPeriod.objects.get(pk=2),prizeAmount="22").save()
+        PaidOut(agreementPeriod=AgreementPeriod.objects.get(pk=2),prizeAmount="24").save()
